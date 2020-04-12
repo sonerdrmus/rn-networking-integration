ddd7bca409cccba4f07e532788aebe3040a24198
Soner Durmuş <sonerdrmus@gmail.com>
2020-04-13 02:12:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import { Component } from 'react'
+import { AppRegistry, ScrollView } from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
